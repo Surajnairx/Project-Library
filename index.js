910d9c768f9c720ff53335a9d7eaa38841101160
Rinzler <Rinzler@123>
2024-04-07 13:35:03 +0530

    Final form element changes

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const addBook = document.querySelector("#add");
 const form = document.querySelector("#form");
 const subBtn = document.querySelector("#submit");
+const canBtn = document.querySelector("#cancel");
+const library = [];
+
+canBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  form.style.display = "none";
+});
 
 addBook.addEventListener("click", () => {
   if (form.style.display == "" || form.style.display == "none") {
@@ -15,5 +22,29 @@ subBtn.addEventListener("click", (event) => {
   const title = document.querySelector("#title").value;
   const author = document.querySelector("#author").value;
   const pages = document.querySelector("#pages").value;
-  console.log(title, author, pages);
+  addBookfn(title, author, pages);
 });
+
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+}
+
+function addBookfn(title, author, pages) {
+  const book = new Book(title, author, pages);
+  library.push(book);
+  const display = document.createElement("div");
+  const h3 = document.createElement("h5");
+  h5.textContent = `Title Name : ${title}`;
+  const p1 = document.createElement("p");
+  p1.textContent = `Author Name :${author}`;
+  const p2 = document.createElement("p");
+  p2.textContent = `Pages :${pages}`;
+  display.setAttribute("class", "book-display");
+  display.appendChild(h5);
+  display.appendChild(p1);
+  display.appendChild(p2);
+  const mainDisplay = document.querySelector(".display-block");
+  mainDisplay.appendChild(display);
+}
